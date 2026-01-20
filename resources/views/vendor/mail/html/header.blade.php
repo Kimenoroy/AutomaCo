76cb4ae07a3a4e5bcbdc428187a06058e1db161b
KimeNoroy <villegasfernando021@gmail.com>
2026-01-19 18:41:26 -0600

    disenios de correo cambiado

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,12 @@
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">      
+            @if(false) 
+                {{-- OPCIÓN A: IMAGEN (Recomendada) --}}
+                <img src="https://tusitio.com/logo-email.png" class="logo" alt="AutomaCo Logo" height="40">
+            @else
+                
+            @endif
+        </a>
+    </td>
+</tr>
