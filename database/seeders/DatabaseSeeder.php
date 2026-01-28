34e35e707f1792ca7f468ba553e96183c335273f
Josueek <abdelmario351@gmail.com>
2026-01-27 22:17:07 -0600

    :fire: feat(): datos base en BD

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,21 +5,15 @@ namespace Database\Seeders;
 use App\Models\User;
 use Database\Seeders\EmailProviderSeeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\ActivationCode;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // Seedear proveedores de email
-        $this->call([
-            EmailProviderSeeder::class,
-        ]);
+    /**Seed the application's database.*/
+  public function run(): void{// Seedear proveedores de email$this->call([EmailProviderSeeder::class,]);
 
         // User::factory(10)->create();
 
@@ -29,6 +23,19 @@ class DatabaseSeeder extends Seeder
         'password' => bcrypt('password123'),
         ]);
 
-        
+        // 3. Usuario de Prueba (Inactivo)
+        $userInactivo = User::factory()->create([
+            'name' => 'Usuario Nuevo',
+            'email' => 'nuevo@prueba.com',
+            'password' => bcrypt('password123'),
+            'is_active' => false,
+        ]);
+
+        ActivationCode::create([
+            'user_id' => $userInactivo->id,
+            'code_hash' => hash('sha256', 'CODIGO2026'),
+            'is_used' => false,
+        ]);
+
     }
 }
